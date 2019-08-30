a15ed3112de06c51a9242bf362245171bb24c50c
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-08-30 11:59:58 +1000

    correction to comply with the latest version of libtransport

--- src/lib.rs
+++ src/lib.rs
@@ -101,16 +101,17 @@ impl<Data> Drop for TCPtransport<Data> {
     }
 }
 
-impl<Id, Pe, Data: 'static, E, PL> Transport<Id, Data, E, PL> for TCPtransport<Data>
+impl<Id, Pe, Data: 'static, E, PL> Transport<Id, Data, E, PL, TCPtransportCfg<Data>>
+    for TCPtransport<Data>
 where
     Data: Serialize + DeserializeOwned + Send + Clone,
     Id: PeerId,
     Pe: Peer<Id>,
     PL: PeerList<Id, E, P = Pe>,
 {
-    type Configuration = TCPtransportCfg<Data>;
+    //type Configuration = TCPtransportCfg<Data>;
 
-    fn new(mut cfg: Self::Configuration) -> Self {
+    fn new(mut cfg: TCPtransportCfg<Data>) -> Self {
         let (tx, rx) = mpsc::channel();
         cfg.set_quit_rx(rx);
         let cfg_mutexed = Arc::new(Mutex::new(cfg));
